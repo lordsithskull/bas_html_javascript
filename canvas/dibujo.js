456e1db7f170f32f4d21652c51c122f85c9b98a6
Miguel Sanchez <miguelse2@hotmail.com>
2021-11-28 23:22:47 -0600

    Se crea la funcion dibujarLinea

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,9 +1,13 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 //console.log(lienzo); comando usado para probar lienzo
-lienzo.beginPath();
-lienzo.stokeStyle = "red";
-lienzo.moveTo(100,100); //punto de origen
-lienzo.lineTo(200,200); //punto final
-lienzo.stroke(); 
-lienzo.closePath();//cierra el camino de la linea
+//dentro de la funcion colocamos el código para generar una  línea
+function dibujarLinea(color,xinicial, yinicial,xfinal,yfinal)
+{
+    lienzo.beginPath();
+    lienzo.stokeStyle = color;
+    lienzo.moveTo(xinicial,yinicial); //punto de origen
+    lienzo.lineTo(xfinal,yfinal); //punto final
+    lienzo.stroke(); 
+    lienzo.closePath();//cierra el camino de la linea
+}
