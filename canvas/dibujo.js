aec70691a3427acad911a80ce84abf310fed7067
Miguel Sanchez <miguelse2@hotmail.com>
2021-11-28 23:30:06 -0600

    Se corrige la linea 10: lienzo.strokeStyle

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,11 +1,13 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-//console.log(lienzo); comando usado para probar lienzo
+//console.log(lienzo); //comando usado para probar lienzo
 //dentro de la funcion colocamos el código para generar una  línea
+dibujarLinea("pink",10,300,220,10);
+dibujarLinea("yellow",300,10,10,220);
 function dibujarLinea(color,xinicial, yinicial,xfinal,yfinal)
 {
     lienzo.beginPath();
-    lienzo.stokeStyle = color;
+    lienzo.strokeStyle = color;
     lienzo.moveTo(xinicial,yinicial); //punto de origen
     lienzo.lineTo(xfinal,yfinal); //punto final
     lienzo.stroke(); 
