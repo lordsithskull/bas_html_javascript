97a77e40b2d35546c2b0973e411786bafff8da73
Miguel Sanchez <miguelse2@hotmail.com>
2021-12-04 23:03:23 -0600

    e genera segundo trazo del dibujo en color verde, además se colocan los bordes de la caja

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -12,9 +12,19 @@ for (l; l < lineas; l++)
     yi = l * 10;
     xf = ( l + 1) * 10 ;
 }
-
-dibujarLinea("black",1,1,1,300)
+l=0;
+for (l; l < lineas; l++)
+{
+    dibujarLinea("#15861d",300,yi,xf,0);
+    console.log("linea " +l);
+    yi = l * 10;
+    xf = ( l + 1) * 10 ;
+}
+dibujarLinea("black",1,1,1,299)
+dibujarLinea("black",1,1,299,1)
 dibujarLinea("black",1,299,299,299)
+dibujarLinea("black",299,299,299,1)
+
 
 function dibujarLinea(color,xinicial, yinicial,xfinal,yfinal)
 {
