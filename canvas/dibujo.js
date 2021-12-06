486d8de7a07814d6b05881e9101952db0a33efb9
Miguel Sanchez <miguelse2@hotmail.com>
2021-12-05 18:23:58 -0600

    Se genera dibujo esquina inferior derecha

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -3,23 +3,32 @@ var lienzo = d.getContext("2d");
 //console.log(lienzo); //comando usado para probar lienzo
 //dentro de la funcion colocamos el código para generar una  línea
 var lineas = 30; 
-var l = 0, yi , xf; 
+var l, xi, yi , xf,yf ; 
 
-for (l; l < lineas; l++)
+for (l = 0 ; l < lineas; l++) //forma inferior izquierda 
 {
     dibujarLinea("#AAF",0,yi,xf,300);
     console.log("linea " +l);
     yi = l * 10;
     xf = ( l + 1) * 10 ;
 }
-l=0;
-for (l; l < lineas; l++)
+
+for (l = 0; l < lineas; l++) //forma inferior derecha
+{
+    dibujarLinea("#15861d",xi,300,300,yf);
+    console.log("linea " +l);
+    xi = l * 10;
+    yf = 300 - ((l+1)*10);
+}
+
+for (l = 0; l < lineas; l++) //forma superior derecha
 {
     dibujarLinea("#15861d",300,yi,xf,0);
     console.log("linea " +l);
     yi = l * 10;
     xf = ( l + 1) * 10 ;
 }
+
 dibujarLinea("black",1,1,1,299)
 dibujarLinea("black",1,1,299,1)
 dibujarLinea("black",1,299,299,299)
