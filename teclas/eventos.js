cddbe4f5df4a59b87f3b4ea99d8466e30cb84602
Miguel Sanchez <miguelse2@hotmail.com>
2021-12-16 02:06:38 -0600

    Se prepara documento en html, en el archivo ventos js se prepara el objeto teclas y se genera el primer codigo para verificar las teclas presionadas

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -1,1 +1,15 @@
-alert("Todo bien");
+var teclas = { //SE GENERA UN OBJETO USANDO NOTACION DE JSON
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39 
+};
+
+console.log(teclas);
+
+document.addEventListener("keyup", dibujarTeclado); //se agregag un listener para detectar teclas presionadas
+
+function dibujarTeclado(evento) //Se genera el primer evento 
+{
+    console.log(evento.keyCode);    
+}
