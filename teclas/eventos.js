1fb0710d1f4a20466b6c802bee029d5e25ec8158
Miguel Sanchez <miguelse2@hotmail.com>
2021-12-18 21:37:32 -0600

    "Se agrega area_de_dibujo como id para trabajo"

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -5,9 +5,20 @@ var teclas = { //SE GENERA UN OBJETO USANDO NOTACION DE JSON
     RIGHT: 39 
 };
 
-console.log(teclas);
-
 document.addEventListener("keyup", dibujarTeclado); //se agregag un listener para detectar teclas presionadas
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getContext("2d");
+dibujarLinea("red",100,100,200,200,papel); //Linea de prueba para verificar que canvas funciona
+
+function dibujarLinea(color,xinicial, yinicial,xfinal,yfinal, lienzo)
+    {
+        lienzo.beginPath();
+        lienzo.strokeStyle = color;
+        lienzo.moveTo(xinicial,yinicial); //punto de origen
+        lienzo.lineTo(xfinal,yfinal); //punto final
+        lienzo.stroke(); 
+        lienzo.closePath();//cierra el camino de la linea
+    }
 
 function dibujarTeclado(evento) //Se genera el primer evento 
 {
