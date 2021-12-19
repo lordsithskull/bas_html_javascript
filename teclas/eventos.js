340f501ab1cfd91519846fb99bdb85f4e8688ee3
Miguel Sanchez <miguelse2@hotmail.com>
2021-12-19 02:44:10 -0600

    Se agrega funcion de dibujar lineas dependiendo la tecla

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -8,13 +8,15 @@ var teclas = { //SE GENERA UN OBJETO USANDO NOTACION DE JSON
 document.addEventListener("keyup", dibujarTeclado); //se agregag un listener para detectar teclas presionadas
 var cuadrito = document.getElementById("area_de_dibujo");
 var papel = cuadrito.getContext("2d");
-dibujarLinea("red",100,100,200,200,papel); //Linea de prueba para verificar que canvas funciona
+var x = 150, y = 150;
+dibujarLinea("red",149,149,151,151,papel); //Linea de prueba para verificar que canvas funciona
 
 function dibujarLinea(color,xinicial, yinicial,xfinal,yfinal, lienzo)
     {
         lienzo.beginPath();
         lienzo.strokeStyle = color;
         lienzo.moveTo(xinicial,yinicial); //punto de origen
+        lienzo.lineWidth = 3; //ancho de la linea en px
         lienzo.lineTo(xfinal,yfinal); //punto final
         lienzo.stroke(); 
         lienzo.closePath();//cierra el camino de la linea
@@ -22,22 +24,32 @@ function dibujarLinea(color,xinicial, yinicial,xfinal,yfinal, lienzo)
 
 function dibujarTeclado(evento) //Se genera el primer evento 
 {
+    var colorcito = "blue";
+    var movimiento = 10;
     switch(evento.keyCode)
-    {
+    {        
         case teclas.UP:
             console.log("arriba");
+            dibujarLinea(colorcito , x, y, x, y - movimiento);
+            y = y - movimiento;
             break;
 
         case teclas.DOWN:
+            dibujarLinea(colorcito , x, y, x, y + movimiento);
+            y = y + movimiento;
             console.log("abajo");
             break;
 
         case teclas.LEFT:
             console.log("izquierda");
+            dibujarLinea(colorcito , x, y, x - movimiento, y );
+            x = x - movimiento;
             break;
             
         case teclas.RIGHT:
             console.log("derecha");
+            dibujarLinea(colorcito , x, y, x + movimiento, y );
+            x = x + movimiento;
             break;
         
         default:
