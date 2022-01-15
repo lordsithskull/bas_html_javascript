debad64eec57f437ee34dbf83f2c03e94e5b7672
Miguel Sanchez <miguelse2@hotmail.com>
2022-01-15 01:28:27 -0600

    Se genera el canvas

--- villa/platzi.js
+++ villa/platzi.js
@@ -1,9 +1,16 @@
-var z = aleatorio(10,20);
-document.write(z);
+var z;
+
+
+for (i = 0; i < 10; i++)
+{
+    z = aleatorio(-2,4);
+    document.write(z +", ");
+}
+
 
 function aleatorio(min, maxi)
 {
     var resultado;
-    resultado = Math.floor (Math.random() * (maxi - min + 1)) + min; //Math.floor redondeo hacia abajo, Maht.
+    resultado = Math.ceil (Math.random() * (maxi - min + 1)) + min; //Math.floor redondeo hacia abajo, Maht.
     return resultado;
 }
