336aa541fa1b930f9d20e4db27452747576dde83
Miguel Sanchez <miguelse2@hotmail.com>
2022-01-15 01:59:06 -0600

    Generacion de código para cargar cerdo, vaca y pollo en el canvas

--- villa/platzi.js
+++ villa/platzi.js
@@ -2,13 +2,40 @@ var vp = document.getElementById("villaPlatzi"); //preparacion del canvas
 var papel = vp.getContext("2d"); //contexto del canvas
 var mapa = "tile.png"; //ruta del "mapa"
 
-var imagen = new Image();// Se genera un nuevo objeto Imagen
-imagen.src = mapa; //Ruta del mapa
-imagen.addEventListener("load",dibujar);//Listener para la carga del objeto
+var fondo = new Image();// Se genera un nuevo objeto Imagen, el cual es el fondo
+fondo.src = mapa; //Ruta del mapa
+fondo.addEventListener("load",dibujarFondo);//Listener para la carga del objeto
 
-function dibujar()
+var vaca = new Image();
+vaca.src = "vaca.png";
+vaca.addEventListener("load",dibujarVaca);
+
+var cerdo = new Image();
+cerdo.src = "cerdo.png";
+cerdo.addEventListener("load",dibujarCerdo);
+
+var pollo = new Image();
+pollo.src = "pollo.png";
+pollo.addEventListener("load",dibujarPollo);
+
+function dibujarFondo()
 {
-    papel.drawImage(imagen,0,0);//Se dibuja la imagen en la posicion  (x,y)
+    papel.drawImage(fondo,0,0);//Se dibuja la imagen en la posicion  (x,y)
+}
+
+function dibujarVaca()
+{
+    papel.drawImage(vaca,0,0);
+}
+
+function dibujarCerdo()
+{
+    papel.drawImage(cerdo,0,0);
+}
+
+function dibujarPollo()
+{
+    papel.drawImage(Pollo,0,0);
 }
 
 function aleatorio(min, maxi)
