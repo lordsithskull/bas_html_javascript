c63d043a537d7cb265c2c78d96402deefb24d5c6
Miguel Sanchez <miguelse2@hotmail.com>
2022-01-16 18:40:24 -0600

    Se genera objeto fondo y se comienzan a crear las nuevas funciones

--- villa/platzi.js
+++ villa/platzi.js
@@ -1,21 +1,30 @@
 var vp = document.getElementById("villaPlatzi"); //preparacion del canvas
 var papel = vp.getContext("2d"); //contexto del canvas
-var mapa = "tile.png"; //ruta del "mapa"
+
+var fondo = {
+    url:"tile.png", //ruta del "mapa"
+    cargaOK = false
+};
 
 var vaca = {
     url:"vaca.png",
     cargaOK = false
 };
 
-var fondo = new Image();// Se genera un nuevo objeto Imagen, el cual es el fondo
-fondo.src = mapa; //Ruta del mapa
-fondo.addEventListener("load",cargarFondo);//Listener para la carga del objeto
+fondo = new Image();// Se genera un nuevo objeto Imagen, el cual es el fondo
+fondo.imagen.src = fondo.url; //Ruta del mapa
+fondo.imagen.addEventListener("load",cargarFondo);//Listener para la carga del objeto
 
 vaca.imagen = new Image();
-vaca.imagen.src = "vaca.png";
+vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener("load",cargarVacas);
 
-function dibujarFondo()
+function cargarFondo()
+{
+    fondo.cargaOK = true;
+}
+
+function dibujar()
 {
     papel.drawImage(fondo,0,0);//Se dibuja la imagen en la posicion  (x,y)
 }
