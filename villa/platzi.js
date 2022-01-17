a2e3b3f51ce885ad22fe1768bfca2c55b8f747de
Miguel Sanchez <miguelse2@hotmail.com>
2022-01-16 18:25:19 -0600

    Se sustituye var vaca por "objeto literal" vaca.

--- villa/platzi.js
+++ villa/platzi.js
@@ -2,41 +2,24 @@ var vp = document.getElementById("villaPlatzi"); //preparacion del canvas
 var papel = vp.getContext("2d"); //contexto del canvas
 var mapa = "tile.png"; //ruta del "mapa"
 
+var vaca = {
+    url:"vaca.png",
+    cargaOK = false
+};
+
 var fondo = new Image();// Se genera un nuevo objeto Imagen, el cual es el fondo
 fondo.src = mapa; //Ruta del mapa
-fondo.addEventListener("load",dibujarFondo);//Listener para la carga del objeto
+fondo.addEventListener("load",cargarFondo);//Listener para la carga del objeto
 
-var vaca = new Image();
-vaca.src = "vaca.png";
-vaca.addEventListener("load",dibujarVaca);
-
-var cerdo = new Image();
-cerdo.src = "cerdo.png";
-cerdo.addEventListener("load",dibujarCerdo);
-
-var pollo = new Image();
-pollo.src = "pollo.png";
-pollo.addEventListener("load",dibujarPollo);
+vaca.imagen = new Image();
+vaca.imagen.src = "vaca.png";
+vaca.imagen.addEventListener("load",cargarVacas);
 
 function dibujarFondo()
 {
     papel.drawImage(fondo,0,0);//Se dibuja la imagen en la posicion  (x,y)
 }
 
-function dibujarVaca()
-{
-    papel.drawImage(vaca,50,20);
-}
-
-function dibujarCerdo()
-{
-    papel.drawImage(cerdo,30,60);
-}
-
-function dibujarPollo()
-{
-    papel.drawImage(pollo,20,80);
-}
 
 function aleatorio(min, maxi)
 {
