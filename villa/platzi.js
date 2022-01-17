5426aa66b2b2eafcac51399a86ba5621bcc05f9e
Miguel Sanchez <miguelse2@hotmail.com>
2022-01-16 20:04:37 -0600

    Primera depuración de código

--- villa/platzi.js
+++ villa/platzi.js
@@ -2,31 +2,39 @@ var vp = document.getElementById("villaPlatzi"); //preparacion del canvas
 var papel = vp.getContext("2d"); //contexto del canvas
 
 var fondo = {
-    url:"tile.png", //ruta del "mapa"
-    cargaOK = false
+    url:"tile.png",
+    cargaOK: false
 };
 
 var vaca = {
     url:"vaca.png",
-    cargaOK = false
+    cargaOK: false
 };
 
 fondo = new Image();// Se genera un nuevo objeto Imagen, el cual es el fondo
-fondo.imagen.src = fondo.url; //Ruta del mapa
+fondo.imagen.src = fondo.url;
 fondo.imagen.addEventListener("load",cargarFondo);//Listener para la carga del objeto
 
 vaca.imagen = new Image();
-vaca.imagen.src = vaca.url;
-vaca.imagen.addEventListener("load",cargarVacas);
 
 function cargarFondo()
 {
     fondo.cargaOK = true;
+    dibujar();
+}
+
+function cargarVaca()
+{
+    vaca.cargaOK = true;
+    dibujar();
 }
 
 function dibujar()
 {
-    papel.drawImage(fondo,0,0);//Se dibuja la imagen en la posicion  (x,y)
+    if (fondo.cargaOK == true)
+    {
+        papel.drawImage(fondo.imagen,0,0);
+    }
 }
 
 
