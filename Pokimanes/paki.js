cf0500689c344ebcb8f437aee96ef8d4f74d570f
Miguel Sanchez <miguelse2@hotmail.com>
2022-01-28 01:35:51 -0600

    Se generan los primeros onjetos pakiman

--- Pokimanes/paki.js
+++ Pokimanes/paki.js
@@ -1,11 +1,19 @@
 class Pakiman //Se genera la clase pakiman
 { 
-    constructor(nombre) //Constrcutor de la clase Pakiman
+    constructor(n,v,a) //Constrcutor de la clase Pakiman
         {
-            this.nombre = nombre;
+            this.nombre = n;
+            this.vida = v;
+            this.ataque = a;
             this.tipo = "tierra";
+        }
+        hablar()
+        {
+            alert(this.nombre);
         }    
 }
 
-var cauchin = new Pakiman("Cauchin");
-console.log(cauchin);
+var cauchin = new Pakiman("Cauchin", 100, 30);
+var pokacho = new Pakiman("Pokacho", 80, 50);
+var tocinauro = new Pakiman("Tocinauro", 120, 40);
+console.log(cauchin, pokacho, tocinauro);
