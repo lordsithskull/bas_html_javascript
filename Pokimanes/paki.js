e3bf34e7e970ca5ded3b172ddb9f0bb504beff07
Miguel Sanchez <miguelse2@hotmail.com>
2022-01-28 02:14:19 -0600

    Se agrega la funcion mostrar

--- Pokimanes/paki.js
+++ Pokimanes/paki.js
@@ -1,19 +1,35 @@
+var imagenes =[]; //Se genera array asociativo, el cual tambíen será un objeto literal, este es nuestro diccionario
+imagenes["Cauchin"] = "vaca.png";
+imagenes["Pokacho"] = "pollo.png";
+imagenes["Tocinauro"] = "cerdo.png";
+
 class Pakiman //Se genera la clase pakiman
 { 
     constructor(n,v,a) //Constrcutor de la clase Pakiman
         {
+            this.imagen = new Image();
             this.nombre = n;
             this.vida = v;
             this.ataque = a;
-            this.tipo = "tierra";
+            
+            this.imagen.src = imagenes[this.nombre];
         }
         hablar()
         {
             alert(this.nombre);
-        }    
+        }
+        
+        mostrar()
+        {
+            document.body.appendChild(this.imagen);
+        }
 }
 
 var cauchin = new Pakiman("Cauchin", 100, 30);
 var pokacho = new Pakiman("Pokacho", 80, 50);
 var tocinauro = new Pakiman("Tocinauro", 120, 40);
 console.log(cauchin, pokacho, tocinauro);
+
+tocinauro.mostrar();
+pokacho.mostrar();
+cauchin.mostrar();
