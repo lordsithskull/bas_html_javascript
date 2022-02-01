905b65fb2031359458b8795d6b6520c5cf60a693
Miguel Sanchez <miguelse2@hotmail.com>
2022-01-31 23:31:15 -0600

    Se genera archivo exclusivo para la clase Pakiman

--- Pokimanes/pakiman.js
+++ Pokimanes/pakiman.js
@@ -1,8 +1,3 @@
-var imagenes =[]; //Se genera array asociativo, el cual tambíen será un objeto literal, este es nuestro diccionario
-imagenes["Cauchin"] = "vaca.png";
-imagenes["Pokacho"] = "pollo.png";
-imagenes["Tocinauro"] = "cerdo.png";
-
 class Pakiman //Se genera la clase pakiman
 { 
     constructor(n,v,a) //Constrcutor de la clase Pakiman
@@ -29,12 +24,3 @@ class Pakiman //Se genera la clase pakiman
             
         }
 }
-
-var cauchin = new Pakiman("Cauchin", 100, 30);
-var pokacho = new Pakiman("Pokacho", 80, 50);
-var tocinauro = new Pakiman("Tocinauro", 120, 40);
-console.log(cauchin, pokacho, tocinauro);
-
-tocinauro.mostrar();
-pokacho.mostrar();
-cauchin.mostrar();
