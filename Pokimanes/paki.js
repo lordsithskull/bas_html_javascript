0f2376103b13c80c8064fb51d667d171d8550426
Miguel Sanchez <miguelse2@hotmail.com>
2022-02-02 23:55:54 -0600

    Se cambian las variables por uso de psuh para generar la coleccion

--- Pokimanes/paki.js
+++ Pokimanes/paki.js
@@ -3,15 +3,8 @@ imagenes["Cauchin"] = "vaca.png";
 imagenes["Pokacho"] = "pollo.png";
 imagenes["Tocinauro"] = "cerdo.png";
 
-
-
-var cauchin = new Pakiman("Cauchin", 100, 30);
-var pokacho = new Pakiman("Pokacho", 80, 50);
-var tocinauro = new Pakiman("Tocinauro", 120, 40);
-console.log(cauchin, pokacho, tocinauro);
-
 var coleccion = [];
-coleccion.push(cauchin);
-coleccion.push(pokacho);
-coleccion.push(tocinauro);
+coleccion.push(new Pakiman("Cauchin", 100, 30) );
+coleccion.push(new Pakiman("Pokacho", 80, 50) );
+coleccion.push(new Pakiman("Tocinauro", 120, 40) );
 console.log(coleccion);
