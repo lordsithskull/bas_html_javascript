2d03f471a3308e69a5282206fd1d71ec54b7cf40
Miguel Sanchez <miguelse2@hotmail.com>
2022-02-06 03:13:30 -0600

    Se genera los objetos billete dentro de un array

--- cajero/atm.js
+++ cajero/atm.js
@@ -1,4 +1,5 @@
-var billete50 = new Billete(50, 10,);
-var billete20 = new Billete(20, 10,);
-var billete10 = new Billete(10, 10,);
-console.log(billete50,billete20,billete10);
+var dinero = [];
+dinero.push(new Billete(50, 10) );
+dinero.push(new Billete(20, 10) );
+dinero.push(new Billete(10, 10) );
+console.log(dinero[0]);
