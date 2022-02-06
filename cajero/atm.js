37529a3a6ca6e13b0c6b5a945f0b283e7dad7599
Miguel Sanchez <miguelse2@hotmail.com>
2022-02-06 03:03:15 -0600

    Se genera la clase Billete

---
+++ cajero/atm.js
@@ -0,0 +1,4 @@
+var billete50 = new Billete(50, 10,);
+var billete20 = new Billete(20, 10,);
+var billete10 = new Billete(10, 10,);
+console.log(billete50,billete20,billete10);
---
+++ cajero/billete.js
@@ -0,0 +1,8 @@
+class Billete
+{
+    constructor(v,c)
+    {
+        this.valor = v;
+        this.cantidad = c;
+    }
+}
