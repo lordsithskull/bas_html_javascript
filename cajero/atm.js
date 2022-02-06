169e6054cd321e63069fdb9e9ad2ece844e08060
Miguel Sanchez <miguelse2@hotmail.com>
2022-02-06 17:55:22 -0600

    Se cambia el nombre de la variable a caja

--- cajero/atm.js
+++ cajero/atm.js
@@ -1,5 +1,5 @@
-var dinero = [];
-dinero.push(new Billete(50, 10) );
-dinero.push(new Billete(20, 10) );
-dinero.push(new Billete(10, 10) );
+var caja = [];
+caja.push(new Billete(50, 10) );
+caja.push(new Billete(20, 10) );
+caja.push(new Billete(10, 10) );
 console.log(dinero[0]);
