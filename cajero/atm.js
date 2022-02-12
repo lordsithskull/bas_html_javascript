755153d170a41070963cf5b41b10396b72fba35b
Miguel Sanchez <miguelse2@hotmail.com>
2022-02-12 01:36:27 -0600

    Primera versión del código de cajero

--- cajero/atm.js
+++ cajero/atm.js
@@ -1,5 +1,34 @@
-var caja = [];
-caja.push(new Billete(50, 10) );
-caja.push(new Billete(20, 10) );
-caja.push(new Billete(10, 10) );
-console.log(dinero[0]);
+var caja = [], entregado = [];
+caja.push(new Billete(50, 1) );
+caja.push(new Billete(20, 20) );
+caja.push(new Billete(10, 2) );
+
+
+var dinero = 160, totalCaja = 0;
+var division = 0, papeles = 0 ;
+
+
+for(b of caja)
+{
+    if (dinero > 0)
+    {
+        division = Math.floor(dinero / b.valor);
+        if (division > b.cantidad)
+            {
+                papeles = b.cantidad;
+            }
+        else
+        {
+            papeles = division;
+        }
+        entregado.push(new Billete(b.valor,papeles) );
+        dinero = dinero - (b.valor * papeles);
+    }
+}
+    
+for(entrega of entregado)
+    {
+        entrega.mostrar();
+        console.log(entrega);
+    }
+
